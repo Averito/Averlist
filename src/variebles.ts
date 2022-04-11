44206084318f87ce1a3934f1afe314e59b53b57e
Avenant <avenantmar@gmail.com>
2022-04-11 12:45:27 +0300

    api urls fixed

--- src/variebles.ts
+++ src/variebles.ts
@@ -1,6 +1,7 @@
-// export const MY_API_URI = 'https://www.averlist.xyz/api'
-export const MY_API_URI = 'http://localhost:8080'
-export const MY_URI = 'http://localhost:8080/users/me/avatar/'
+export const MY_API_URI = 'https://www.averlist.xyz/api'
+export const MY_URI = 'https://www.averlist.xyz/api/users/me/avatar/'
+// export const MY_API_URI = 'http://localhost:8080'
+// export const MY_URI = 'http://localhost:8080/users/me/avatar/'
 
 export const ANILIBRIA_API_URI = 'https://api.anilibria.tv/v2'
 export const ANILIBRIA_URI = 'https://dl-20211030-963.anilib.top/'
