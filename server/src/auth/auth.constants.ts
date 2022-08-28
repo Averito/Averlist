6647524c491ec0025232776aa552be4765fd2ca5
Averito <avenantmar@gmail.com>
2022-08-29 01:22:48 +0300

    edit jwtPayload

--- server/src/auth/auth.constants.ts
+++ server/src/auth/auth.constants.ts
@@ -1,5 +1,7 @@
 export const USER_WAS_FOUND = 'Пользователь с таким email уже существует'
 export const USER_NOT_FOUND = 'Пользователя с таким email не существует'
+export const USER_WITH_THIS_ID_NOT_FOUND =
+	'Пользователя с таким id не существует'
 export const PASSWORD_WRONG = 'Неправильный пароль'
 export const REFRESH_TOKEN_WRONG = 'Токен не верный'
 export const ACTIVATE_LINK_NOT_VALID = 'Ключ активации не валидный'
