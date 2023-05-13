34381477ac51b90e324df69f74757a523b1d719a
Averito <avenantmar@gmail.com>
2023-05-13 17:18:09 +0300

    fix nodemailer.config.ts

--- server/src/config/nodemailer.config.ts
+++ server/src/config/nodemailer.config.ts
@@ -3,16 +3,15 @@ import { MailerOptions } from '@nestjs-modules/mailer'
 
 export const getNodemailerConfig = (
 	configService: ConfigService
-): MailerOptions => {
-	return {
-		transport: {
-			host: configService.get('MAILER_HOST'),
-			port: configService.get('MAILER_PORT'),
-			secure: true,
-			auth: {
-				user: configService.get('MAILER_AUTH_USER'),
-				pass: configService.get('MAILER_AUTH_PASS')
-			}
+): MailerOptions => ({
+	transport: {
+		host: configService.get('MAILER_HOST'),
+		port: configService.get('MAILER_PORT'),
+		secure: true,
+		pool: true,
+		auth: {
+			user: configService.get('MAILER_AUTH_USER'),
+			pass: configService.get('MAILER_AUTH_PASS')
 		}
 	}
-}
+})
